215844ee4fd0d516d21eb4357985bca04405b93e
Jaemok Jeong <jmjeong@gmail.com>
2016-05-10 20:25:20 +0900

    v2 environment

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/coc-dev'
+    uri: 'mongodb://localhost/coc2-dev'
   },
 
   seedDB: false
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -11,13 +11,13 @@ module.exports = {
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
             process.env.PORT ||
-            8900,
+            8901,
 
   // MongoDB connection options
   mongo: {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/coc'
+            'mongodb://localhost/coc2'
   }
 };
